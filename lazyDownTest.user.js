8215de71c416e293daa8e8bde94b6156d264b23e
gb_2312 <iamgqb@gmail.com>
2013-09-21 12:16:32 +0800

    小改动
    去掉了加心的，加上虾米的

--- lazyDownTest.user.js
+++ lazyDownTest.user.js
@@ -4,26 +4,32 @@
 // ==/UserScript==
 
     var doc = document;
-if (window.location.href=='http://douban.fm/'){
-    var downButton = doc.createElement('li');
-    downButton.innerHTML = '<a href="javascript:;" id="my_lazydown">下载</a>';
-    doc.getElementById('user_play_record').appendChild(downButton);
-
-    doc.getElementById('my_lazydown').addEventListener('click', function(){
-        var songName = doc.title.slice(0, -7);
-        var url = 'http://music.baidu.com/search?key=' + songName;
-        window.open(url)
-    }, false);
-}
-
-if (window.location.href.indexOf('type=liked')){
-    var lists = doc.querySelectorAll('.info_wrapper');
-    for(var i=0, l=lists.length;i<l;i++){
-        var song_title = lists[i].querySelector('.song_title').innerHTML;
-        var downButton = doc.createElement('a');
-        downButton.href = 'javascript:window.open(\'http://music.baidu.com/search?key='+song_title+'\');'
-        downButton.innerHTML = '下载';
-        lists[i].appendChild(downButton);
+    if (window.location.href=='http://douban.fm/'){
+        var downButton = doc.createElement('li');
+        downButton.innerHTML = '<a href="javascript:;" id="baidu_search">搜索百度</a>';
+        
+        var xiamiButton = doc.createElement('li');
+        xiamiButton.innerHTML = '<a href="javascript:;" id="xiami_search">搜索虾米</a>';       
+        
+        doc.getElementById('user_play_record').appendChild(downButton);
+        doc.getElementById('user_play_record').appendChild(xiamiButton); 
+        
+        doc.getElementById('baidu_search').addEventListener('click', function(){            
+            var url = 'http://music.baidu.com/search?key=' + getSongName();
+            window.open(url);
+        }, false);
+        
+        doc.getElementById('xiami_search').addEventListener('click', function(){
+            var url = 'http://www.xiami.com/search?key=' + getSongName();
+            window.open(url);
+        }, false);
+        
     }
-}
+    
+    var getSongName = function(){
+        return encodeURIComponent(doc.title.slice(0, -14));
+    }
+    console.log('Lazy download loaded');
 
+
+    
